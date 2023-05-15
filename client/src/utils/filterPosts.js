d1fb1ff3147a3d976962bbef23c4e1f6d90ae5f2
Jun Pyo Seo <cooljp95@naver.com>
2023-05-15 15:54:56 +0900

    [FEAT] Filter => menu로 변경 / 정렬 추가

--- client/src/utils/filterPosts.js
+++ client/src/utils/filterPosts.js
@@ -2,13 +2,7 @@ import FILTERS from '../constants/filters';
 
 const filterPosts = (posts, currentFilter) =>
   posts.filter(post =>
-    currentFilter === FILTERS.active
-      ? !post.completed
-      : currentFilter === FILTERS.completed
-      ? post.completed
-      : currentFilter === FILTERS.certified
-      ? post.certified
-      : true
+    currentFilter === FILTERS.active ? !post.completed : currentFilter === FILTERS.completed ? post.completed : true
   ) ?? [];
 
 export default filterPosts;
