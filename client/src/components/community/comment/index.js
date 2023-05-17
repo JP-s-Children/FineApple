b66e4b0a15b0d2b33c74d34ffe816ea728825d7d
Jun Pyo Seo <cooljp95@naver.com>
2023-05-17 14:37:01 +0900

    [feat] likeChip 컴포넌트 추가

--- client/src/components/community/comment/index.js
+++ client/src/components/community/comment/index.js
@@ -1,3 +1,4 @@
 export { default as Comment } from './Comment';
 export { default as CommentSection } from './CommentSection';
 export { default as UsefulCommentChip } from './UsefulCommentChip';
+export { default as LikeChip } from './LikeChip';
