7357140466074667442033735e3e54741ee3ea89
Jun Pyo Seo <cooljp95@naver.com>
2023-05-18 17:22:46 +0900

    [FEAT] 인기 순으로 정렬 추가

--- client/src/utils/sortPosts.js
+++ client/src/utils/sortPosts.js
@@ -4,7 +4,7 @@ const sortPosts = (posts, currentSort) =>
       ? new Date(b.createAt) - new Date(a.createAt)
       : currentSort === 'old'
       ? new Date(a.createAt) - new Date(b.createAt)
-      : a.like - b.like
+      : a.like.length - b.like.length
   );
 
 export default sortPosts;
