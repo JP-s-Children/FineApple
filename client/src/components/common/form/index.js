a611fab1741000635811a7f66d6e2207db62e0b9
sunhwa <sumz.ko@gmail.com>
2023-05-20 16:17:58 +0900

    [REFAC] 사용안하는 컴포넌트 제거

--- client/src/components/common/form/index.js
+++ client/src/components/common/form/index.js
@@ -1,5 +1,4 @@
 export { default as InputWrapper } from './InputWrapper';
 export { default as CountrySelect } from './CountrySelect';
 export { default as BirthDateInput } from './BirthDateInput';
-export { default as DuplicateCheckInput } from './DuplicateCheckInput';
 export { default as PhoneNumberInput } from './PhoneNumberInput';
