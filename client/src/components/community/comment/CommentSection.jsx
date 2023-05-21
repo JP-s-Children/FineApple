fdf1fc088cba7f4f5ed039ffac9137c68113b54a
olhkyle <kylekwon96@gmail.com>
2023-05-21 23:54:45 +0900

    [FIX] comment 추가 시 level 누락 수정 / firebase DB 내 누락 데이터 추가

--- client/src/components/community/comment/CommentSection.jsx
+++ client/src/components/community/comment/CommentSection.jsx
@@ -69,6 +69,7 @@ const CommentSection = ({ postInfo, mutateFns }) => {
           nickName: user.nickName,
           avatarId: user.avatarId,
           content: textEditorContent,
+          level: user.level,
         },
       },
       {
