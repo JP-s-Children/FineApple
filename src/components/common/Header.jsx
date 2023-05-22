76fe2f2101ce6f4596e236915bcfea7aedf497a0
olhkyle <kylekwon96@gmail.com>
2023-05-22 18:24:55 +0900

    [STYLE] Header 컴포넌트 상단 margin 부족으로 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,47 +4,48 @@ import { Container } from '@mantine/core';
 import { Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
-  padding: 11px 0px 11px 0px;
-  width: 100%;
-  min-width: 1024px;
-  border-bottom: 1px solid var(--opacity-border-color);
-  background-color: var(--body-bg-color);
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-  position: sticky;
-  top: 0;
-  z-index: 99;
+	margin-top: 3px;
+	padding: 14px 0px 11px 0px;
+	width: 100%;
+	min-width: 1024px;
+	border-bottom: 1px solid var(--opacity-border-color);
+	background-color: var(--body-bg-color);
+	box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
+	position: sticky;
+	top: 0;
+	z-index: 99;
 `;
 
 const Wrapper = styled(Container)`
-  width: 1024px;
-  min-width: 1024px;
-  display: flex;
-  justify-content: space-between;
-  font-size: 0.75rem;
-  padding: 0px;
+	width: 1024px;
+	min-width: 1024px;
+	display: flex;
+	justify-content: space-between;
+	font-size: 0.75rem;
+	padding: 0px;
 `;
 
 const Title = styled(Link)`
-  font-size: 18px;
-  font-weight: 600;
-  color: var(--font-color);
-  text-decoration: none;
-  margin-left: 20px;
+	font-size: 18px;
+	font-weight: 600;
+	color: var(--font-color);
+	text-decoration: none;
+	margin-left: 20px;
 
-  :hover {
-    color: var(--hover-font-color);
-  }
+	:hover {
+		color: var(--hover-font-color);
+	}
 `;
 
 const MenuWrapper = styled.div`
-  display: flex;
-  gap: 15px;
+	display: flex;
+	gap: 15px;
 `;
 
 const Menu = styled(Title)`
-  padding-top: 0.2rem;
-  font-size: 14px;
-  font-weight: 400;
+	padding-top: 0.2rem;
+	font-size: 14px;
+	font-weight: 400;
 `;
 
 /**
@@ -54,18 +55,18 @@ const Menu = styled(Title)`
  * }} props
  */
 const Header = ({ title, menuList }) => (
-  <HeaderContainer>
-    <Wrapper>
-      <Title to={title.path}>{title.content}</Title>
-      <MenuWrapper>
-        {menuList.map(({ path, content }) => (
-          <Menu key={content} to={path}>
-            {content}
-          </Menu>
-        ))}
-      </MenuWrapper>
-    </Wrapper>
-  </HeaderContainer>
+	<HeaderContainer>
+		<Wrapper>
+			<Title to={title.path}>{title.content}</Title>
+			<MenuWrapper>
+				{menuList.map(({ path, content }) => (
+					<Menu key={content} to={path}>
+						{content}
+					</Menu>
+				))}
+			</MenuWrapper>
+		</Wrapper>
+	</HeaderContainer>
 );
 
 export default Header;
